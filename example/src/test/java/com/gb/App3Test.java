d2269aca673423567716ff55476f2c66a1e572aa
IgorBabikov <babikov_igor@inbox.ru>
2024-11-15 15:06:08 +0300

    1 raz

--- example/src/test/java/com/gb/App3Test.java
+++
@@ -1,7 +0,0 @@
-package com.gb;
-
-public class App3Test {
-    public static void main(String[] args) {
-        System.out.println("ONE");
-    }
-}
--- example/src/test/java/com/gb/AppTest.java
+++ example/src/test/java/com/gb/AppTest.java
@@ -14,6 +14,6 @@ public class AppTest {
      */
     @Test
     public void shouldAnswerWithTrue() {
-        System.out.println("test this place rewrite");;
+        System.out.println("test this place HUY}");;
     }
 }
