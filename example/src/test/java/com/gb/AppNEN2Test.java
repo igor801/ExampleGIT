8cde20a960a66154c81cb9f3c137db9135f53cb8
IgorBabikov <babikov_igor@inbox.ru>
2024-11-15 18:35:04 +0300

    revert all files

---
+++ example/src/test/java/com/gb/AppNEN2Test.java
@@ -0,0 +1,17 @@
+package com.gb;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppNEN2Test {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("test this place HUY22}");;
+    }
+}
